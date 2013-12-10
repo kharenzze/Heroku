06c75e45854c5525b1331578f0b5fba1784d2373
kharenzze <str237@gmail.com>
2013-12-10 01:15:45 +0100

    otro cambio

--- servidor.js
+++ servidor.js
@@ -9,6 +9,6 @@ app.get('/proc', function (req, res) {
 	res.send('No es la portada');
 });
 
-var port=5000;
+var port=process.env.PORT || 5000;
 app.listen(port);
 console.log('Server running at http://127.0.0.1:'+port+'/');
